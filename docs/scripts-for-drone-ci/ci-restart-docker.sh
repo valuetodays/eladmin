fcee919616298c23b31b87d89b2242822783612a
valuetodays <abcdefg20111111@gmail.com>
2025-07-11 16:38:24 +0800

    update docker-compose to docker compose

--- docs/scripts-for-drone-ci/ci-restart-docker.sh
+++ docs/scripts-for-drone-ci/ci-restart-docker.sh
@@ -6,8 +6,8 @@ dc_file=/root/workbench_dir/git-repo/valuetodays-dockers/eladmin/docker-compose.
 log_text=$(docker-compose -f ${dc_file} ps | grep 'java')
 echo "log_text=[$log_text]"
 if [ "$log_text" == "" ]; then
-  docker-compose -f ${dc_file} up -d
+  docker compose -f ${dc_file} up -d
 else
-  docker-compose -f ${dc_file} restart
+  docker compose -f ${dc_file} restart
 fi
 # end
