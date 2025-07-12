0cadc93a113eacd856eb70cb1e10e949c0ebdedd
valuetodays <abcdefg20111111@gmail.com>
2025-07-13 00:14:59 +0800

    use pgsql

--- eladmin-system/src/main/java/me/zhengjie/modules/quartz/config/JobRunner.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/quartz/config/JobRunner.java
@@ -16,7 +16,6 @@
 package me.zhengjie.modules.quartz.config;
 
 import lombok.RequiredArgsConstructor;
-import me.zhengjie.modules.quartz.domain.QuartzJob;
 import me.zhengjie.modules.quartz.repository.QuartzJobRepository;
 import me.zhengjie.modules.quartz.utils.QuartzManage;
 import org.slf4j.Logger;
@@ -24,7 +23,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-import java.util.List;
 
 /**
  * @author Zheng Jie
@@ -44,8 +42,9 @@ public class JobRunner implements ApplicationRunner {
      */
     @Override
     public void run(ApplicationArguments applicationArguments) {
-        List<QuartzJob> quartzJobs = quartzJobRepository.findByIsPauseIsFalse();
-        quartzJobs.forEach(quartzManage::addJob);
+        // todo disable temply
+//        List<QuartzJob> quartzJobs = quartzJobRepository.findByIsPauseIsFalse();
+//        quartzJobs.forEach(quartzManage::addJob);
         log.info("Timing task injection complete");
     }
 }
