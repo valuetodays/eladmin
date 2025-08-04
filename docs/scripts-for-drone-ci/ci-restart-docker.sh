0517bf1cb737d779ec46f71a69a6b8f6a3abc41f
valuetodays <abcdefg20111111@gmail.com>
2025-08-04 14:59:19 +0800

    fix restart logic

--- docs/scripts-for-drone-ci/ci-restart-docker.sh
+++ docs/scripts-for-drone-ci/ci-restart-docker.sh
@@ -2,12 +2,17 @@
 
 # if place the code in .drone.yml : only restart docker, cannot start it up
 # but if place in an sh file: it works all.
+
 dc_file=/root/workbench_dir/git-repo/valuetodays-dockers/eladmin-tmp/docker-compose.yml
-log_text=$(docker compose -f ${dc_file} ps | grep 'java')
+# print result
+docker compose -f ${dc_file} ps
+log_text=$(docker compose -f ${dc_file} ps | grep 'eladmin-tmp-system')
 echo "log_text=[$log_text]"
 if [ "$log_text" == "" ]; then
+  echo "Service not running, starting..."
   docker compose -f ${dc_file} up -d
 else
+  echo "Service is running, restarting..."
   docker compose -f ${dc_file} restart
 fi
 # end
