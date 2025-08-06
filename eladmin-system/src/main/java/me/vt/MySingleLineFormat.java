307994c1383bda7288aacc2950080d5b5daa181f
valuetodays <abcdefg20111111@gmail.com>
2025-08-06 19:57:36 +0800

    add log,

---
+++ eladmin-system/src/main/java/me/vt/MySingleLineFormat.java
@@ -0,0 +1,23 @@
+package me.vt;
+
+import cn.vt.exception.CommonException;
+import com.p6spy.engine.common.P6Util;
+import com.p6spy.engine.spy.appender.SingleLineFormat;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * .
+ *
+ * @author lei.liu
+ * @since 2025-06-17
+ */
+public class MySingleLineFormat extends SingleLineFormat {
+    @Override
+    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
+        String newUrl = StringUtils.substringBefore(url, "?") + "?...";
+        if (url.contains(",122,122,122,")) {
+            throw new CommonException("show error");
+        }
+        return now + "|" + elapsed + "|" + category + "|connection " + connectionId + "|url " + newUrl + "|" + "" + "|" + P6Util.singleLine(sql);
+    }
+}
