6ae4d0568ef24d627803a6ba310c8f3a78f79770
valuetodays <abcdefg20111111@gmail.com>
2025-08-06 20:00:10 +0800

    add log,

--- eladmin-system/src/main/java/me/vt/MySingleLineFormat.java
+++ eladmin-system/src/main/java/me/vt/MySingleLineFormat.java
@@ -15,7 +15,7 @@ public class MySingleLineFormat extends SingleLineFormat {
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
         String newUrl = StringUtils.substringBefore(url, "?") + "?...";
-        if (url.contains(",122,122,122,")) {
+        if (sql.contains(",122,122,122,")) {
             throw new CommonException("show error");
         }
         return now + "|" + elapsed + "|" + category + "|connection " + connectionId + "|url " + newUrl + "|" + "" + "|" + P6Util.singleLine(sql);
