c84826d77b638baa753bc86ec0ad00994f012a68
valuetodays <abcdefg20111111@gmail.com>
2025-09-03 19:27:50 +0800

    rename eladmin-system-quarkus

--- docs/scripts-for-drone-ci/ci-restart-docker.sh
+++ docs/scripts-for-drone-ci/ci-restart-docker.sh
@@ -3,10 +3,10 @@
 # if place the code in .drone.yml : only restart docker, cannot start it up
 # but if place in an sh file: it works all.
 
-dc_file=/root/workbench_dir/git-repo/valuetodays-dockers/eladmin-tmp/docker-compose.yml
+dc_file=/root/workbench_dir/git-repo/valuetodays-dockers/eladmin-quarkus/docker-compose.yml
 # print result
 docker compose -f ${dc_file} ps
-log_text=$(docker compose -f ${dc_file} ps | grep 'eladmin-tmp-system')
+log_text=$(docker compose -f ${dc_file} ps | grep 'eladmin-system-quarkus')
 echo "log_text=[$log_text]"
 if [ "$log_text" == "" ]; then
   echo "Service not running, starting..."
