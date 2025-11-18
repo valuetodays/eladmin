79fce92ee25eac069a13d474ab5a469d4b018d74
valuetodays <abcdefg20111111@gmail.com>
2025-11-18 20:34:50 +0800

    计算popular指数的近30天cci值, use @EmbeddedId

---
+++ module-mybiz/src/main/java/me/vt/modules/mybiz/domain/StockDailyIndicatorPk.java
@@ -0,0 +1,25 @@
+package me.vt.modules.mybiz.domain;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * .
+ *
+ * @author lei.liu
+ * @since 2025-11-18
+ */
+@Data
+public class StockDailyIndicatorPk implements Serializable {
+    @Column(name = "code")
+    @Schema(description = "code")
+    private String code;
+
+    @Column(name = "stat_date")
+    @Schema(description = "统计日期")
+    private LocalDate statDate;
+}
