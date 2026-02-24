62a4cebe79abc92216f6f700703bc1d25905234e
valuetodays <abcdefg20111111@gmail.com>
2026-02-24 11:49:43 +0800

    feat: query stockInfo

--- module-mybiz/src/main/java/me/vt/modules/mybiz/service/dto/StockInfoQueryCriteria.java
+++ module-mybiz/src/main/java/me/vt/modules/mybiz/service/dto/StockInfoQueryCriteria.java
@@ -2,11 +2,15 @@ package me.vt.modules.mybiz.service.dto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import ll.vt.quarkus.commons.base.Operator;
 import ll.vt.quarkus.commons.base.PageIO;
 import ll.vt.quarkus.commons.base.QuerySearch;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import me.vt.common.QuerySearchable;
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 /**
@@ -29,10 +33,24 @@ public class StockInfoQueryCriteria extends PageIO implements QuerySearchable {
     @Schema(description = "区域（上海，深圳，北京，香港）")
     private String region;
 
+    @Schema(description = "是否流行")
+    private Boolean popularFlag;
 
     @Override
     public List<QuerySearch> toQuerySearches() {
         List<QuerySearch> querySearches = new ArrayList<>();
+        if (StringUtils.isNotBlank(code)) {
+            querySearches.add(QuerySearch.of("code", code, Operator.EQ));
+        }
+        if (StringUtils.isNotBlank(shortName)) {
+            querySearches.add(QuerySearch.of("shortName", shortName, Operator.LIKE));
+        }
+        if (StringUtils.isNotBlank(region)) {
+            querySearches.add(QuerySearch.of("region", region, Operator.EQ));
+        }
+        if (Objects.nonNull(popularFlag)) {
+            querySearches.add(QuerySearch.of("popularFlag", String.valueOf(popularFlag), Operator.EQ));
+        }
 
         return querySearches;
     }
