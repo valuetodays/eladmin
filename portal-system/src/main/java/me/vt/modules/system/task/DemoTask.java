2f532eae6435d9f8bd2f4e09b9669b6e3d8c8518
valuetodays <abcdefg20111111@gmail.com>
2026-02-25 15:33:47 +0800

    opt: add DemoTask.java

---
+++ portal-system/src/main/java/me/vt/modules/system/task/DemoTask.java
@@ -0,0 +1,24 @@
+package me.vt.modules.system.task;
+
+import io.quarkus.scheduler.Scheduled;
+import jakarta.enterprise.context.ApplicationScoped;
+import ll.vt.quarkus.commons.base.RunAsync;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * .
+ *
+ * @author lei.liu
+ * @since 2026-02-25
+ */
+@Slf4j
+@ApplicationScoped
+public class DemoTask extends RunAsync {
+    @Scheduled(cron = "*/20 * * * * ?")
+    public void interval1() {
+        super.executeAsync(() -> {
+            log.info("interval1 begins");
+            log.info("interval1 ends");
+        });
+    }
+}
